3085a69b54c4da041202ddadd645afdadbcfe3b1
oga491 <134859974+oga491@users.noreply.github.com>
2023-06-23 14:47:47 +0900

    あ

--- main.py
+++ main.py
@@ -1,39 +1,61 @@
-import math
 import streamlit as st
-import pandas as pd
+import json
+from datetime import datetime
+import pytz
 
-st.text("ヤッハロー")
-#         ↑ガハマすこ
+# 禁止ワードのリスト
+banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
-# 投稿データを保持するリスト
-posts = []
+# ユーザーの投稿内容をチェックする関数
+def check_post_content(title, content):
+    # タイトルと投稿内容の禁止ワードの検出
+    for banned_word in banned_words:
+        if banned_word in title:
+            title = title.replace(banned_word, "＠" * len(banned_word))
+        if banned_word in content:
+            content = content.replace(banned_word, "＠" * len(banned_word))
+    return title, content
 
-# 投稿の作成
-def create_post(title, content):
-    posts.append({"title": title, "content": content})
+def save_post(title, content):
+    now = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+    post = {"title": title, "content": content, "timestamp": now}
+    with open('posts.json', 'a') as file:
+        file.write(json.dumps(post))
+        file.write('\n')
 
-# 投稿の表示
-def show_posts():
-    for post in posts:
-        st.write(f"**{post['title']}**")
-        st.write(post['content'])
-        st.write('---')
+def load_posts():
+    with open('posts.json', 'r') as file:
+        lines = file.readlines()
+        return [json.loads(line) for line in lines]
 
-# 掲示板アプリのタイトル
-st.title("舞チャン")
+def main():
+    st.title("掲示板アプリ")
 
-# 新しい投稿の作成
-st.header("新しい投稿を作成")
-title = st.text_input("スレタイトル")
-content = st.text_area("内容")
-if st.button("作成！"):
-    create_post(title, content)
-    st.success("作成完了！")
+    # 新規投稿の入力
+    new_post_title = st.text_input("タイトル")
+    new_post_content = st.text_area("新規投稿", height=100)
 
-# 投稿一覧の表示
-st.header("スレタイトル一覧")
-if len(posts) == 0:
-    st.info("まだ投稿はありません")
-else:
-    show_posts()
+    # 投稿ボタンが押された場合
+    if st.button("投稿する") and new_post_title and new_post_content:
+        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+        if "＠" in new_post_title or "＠" in new_post_content:
+            st.warning("禁止ワードが含まれています！")
 
+        save_post(new_post_title, new_post_content)
+        st.success("投稿が保存されました！")
+
+    # 保存された投稿の表示
+    posts = load_posts()
+    st.subheader("保存された投稿")
+
+    if not posts:
+        st.info("まだ投稿がありません。")
+    else:
+        for post in posts:
+            st.text(post["title"])
+            st.text(post["content"])
+            st.text("投稿時刻: " + post.get("timestamp", ""))
+            st.markdown("---")
+
+if __name__ == "__main__":
+    main()
