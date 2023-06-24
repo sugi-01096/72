c87c2b0d6d6f1fa4a3fd4a17e4b5cdbf72f884e8
sugi-01096 <nodoguro04@gmail.com>
2023-06-24 09:51:42 +0900

    utl施策

--- maun.py
+++ maun.py
@@ -1,40 +1,45 @@
 import streamlit as st
 import json
+from datetime import datetime
+import pytz
+import urllib.parse
 
 # 禁止ワードのリスト
 banned_words = ["馬鹿", "禁止ワード2", "禁止ワード3"]
 
 # ユーザーの投稿内容をチェックする関数
-def check_post_content(post_content):
-    # 禁止ワードの検出
+def check_post_content(title, content):
+    # タイトルと投稿内容の禁止ワードの検出
     for banned_word in banned_words:
-        if banned_word in post_content:
-            # 禁止ワードを＠に置き換える
-            post_content = post_content.replace(banned_word, "＠" * len(banned_word))
-            return post_content, True  # 禁止ワードが検出された場合は置き換えた投稿内容とTrueを返す
-    return post_content, False  # 禁止ワードが検出されなかった場合は投稿内容とFalseを返す
+        if banned_word in title:
+            title = title.replace(banned_word, "＠" * len(banned_word))
+        if banned_word in content:
+            content = content.replace(banned_word, "＠" * len(banned_word))
+    return title, content
 
 def save_post(title, content):
-    post = {"title": title, "content": content}
+    now = datetime.now(pytz.timezone("Asia/Tokyo")).strftime("%Y-%m-%d %H:%M:%S")
+    post = {"title": title, "content": content, "timestamp": now}
     with open('posts.json', 'a') as file:
-        json.dump(post, file)
+        file.write(json.dumps(post))
         file.write('\n')
 
 def load_posts():
     with open('posts.json', 'r') as file:
-        return [json.loads(line) for line in file]
+        lines = file.readlines()
+        return [json.loads(line) for line in lines]
 
 def main():
     st.title("掲示板アプリ")
 
     # 新規投稿の入力
-    new_post_title = st.text_input("タイトル")
+    
     new_post_content = st.text_area("新規投稿", height=100)
-
+    new_post_title = st.text_input("ページ")
     # 投稿ボタンが押された場合
     if st.button("投稿する") and new_post_title and new_post_content:
-        new_post_content, banned = check_post_content(new_post_content)
-        if banned:
+        new_post_title, new_post_content = check_post_content(new_post_title, new_post_content)
+        if "＠" in new_post_title or "＠" in new_post_content:
             st.warning("禁止ワードが含まれています！")
 
         save_post(new_post_title, new_post_content)
@@ -48,8 +53,10 @@ def main():
         st.info("まだ投稿がありません。")
     else:
         for post in posts:
-            st.text(post["title"])
-            st.text(post["content"])
+            # 各タイトルにリンクを付けて表示
+            post_url = f"<a href='https://wikiwiki.jp/{urllib.parse.quote(post['title'])}'>{post['title']}</a>"
+            st.markdown(post_url, unsafe_allow_html=True)
+            st.write(post['content'])
             st.markdown("---")
 
 if __name__ == "__main__":
