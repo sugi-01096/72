1367cd281c88ec28bb47cdd6e195fd3e6cfbed1d
ko-ki <135305163+ZUMpou@users.noreply.github.com>
2023-10-27 15:09:39 +0900

    kou

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ banned_words = df['禁止ワード'].tolist()
 def check_post_content(title, content):
     # タイトルと投稿内容の禁止ワードの検出
     for banned_word in banned_words:
-        if banned_word in title:
-            title = title.replace(banned_word, "＠" * len(banned_word))
+        if str(banned_word) in title:
+             title = title.replace(banned_word, "＠" * len(banned_word))
         if banned_word in content:
             content = content.replace(banned_word, "＠" * len(banned_word))
     return title, content
