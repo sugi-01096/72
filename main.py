797d89de62151606e79062be179e42fb3c637424
sugi-01096 <132065223+sugi-01096@users.noreply.github.com>
2023-10-27 14:36:37 +0900

    xl

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import urllib.parse
 # 禁止ワードのリスト
 # Excelファイルから読み込む
 def load_banned_words():
-    df = pd.read_csv("禁止ワード.csv")
+    df = pd.read_excel("禁止ワード.xlsx")
     return df['禁止用語'].tolist()
 
 banned_words = load_banned_words()
