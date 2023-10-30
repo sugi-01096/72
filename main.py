46e7acd29ad8e58314b698ab712d8c2d4e224b39
sugi-01096 <nodoguro04@gmail.com>
2023-10-30 20:52:50 +0900

    ア

--- main.py
+++ main.py
@@ -5,25 +5,27 @@ from datetime import datetime
 import pytz
 import urllib.parse
 
-# 禁止ワードのリスト
-# Excelファイルから読み込む
+
+# 禁止ワードをExcelファイルから読み込む
 df = pd.read_excel("banned_list.xlsx", sheet_name=0)
+#禁止ワードをbanned_words に
 banned_words = df['禁止ワード'].tolist()
 banned_words = [str(word) for word in banned_words]
 
 
 # ユーザーの投稿内容をチェックする関数
 def check_post_content(title, content):
-    # タイトルと投稿内容の禁止ワードの検出
+    
     for banned_word in banned_words:
         if str(banned_word) in title:
              title = title.replace(banned_word, "＠" * len(banned_word))
         if banned_word in content:
-            content = content.replace(banned_word, "＠" * len(banned_word))
+            content = content.replace(banned_word, "＠" * len(banned_word))# タイトルと投稿内容の禁止ワードの検出し、禁止ワードがあったら文字を＠に変換
     return title, content
 
-# 以下のコードは変更なし
+    
 def save_post(title, content):
+    #タイムスタンプを設定
     now = datetime.now(pytz.timezone("Asia/Tokyo"))
     now_str = now.strftime("%Y-%m-%d %H:%M:%S")
     post = {"title": title, "content": content, "timestamp": now_str}
@@ -36,18 +38,18 @@ def load_posts():
         lines = file.readlines()
         posts = [json.loads(line.strip()) for line in lines]
         
-        # タイムスタンプを日本時間に変換
-        for post in posts:
+       for post in posts: # タイムスタンプを日本時間に変換
             timestamp = datetime.strptime(post['timestamp'], "%Y-%m-%d %H:%M:%S")
             timestamp = pytz.timezone("Asia/Tokyo").localize(timestamp)
             post['timestamp'] = timestamp.strftime("%Y-%m-%d %H:%M:%S")
 
-        return posts
+         return posts
+       
 
 def main():
     st.title("掲示板アプリ")
 
-    # 新規投稿の入力
+    # ページのタイトルの入力
     new_post_content = st.text_area("管理者以外記述厳禁", height=100)
     new_post_title = st.text_input("ページ")
     
@@ -59,17 +61,16 @@ def main():
 
         save_post(new_post_title, new_post_content)
         st.success("投稿が保存されました！")
-
-    # 保存された投稿の表示
+#↑まで入力の段
+    
     posts = load_posts()
-    st.subheader("保存された投稿")
-
+    st.subheader("保存された投稿")# 保存された投稿の表示
+　　
     if not posts:
-        st.info("まだ投稿がありません。")
+        st.info("まだ投稿がありません。")#何も投稿がないときはまだ投稿がありませんと表示
     else:
         for post in posts:
-            # 各タイトルにリンクを付けて表示
-            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post['title'])}.streamlit.app'>{post['title']}</a>"
+            post_url = f"<a href='https://maichan-bord-{urllib.parse.quote(post['title'])}.streamlit.app'>{post['title']}</a>"# 各タイトルにhttps://maichan-bord-＋title+.streamlit.appになるようにリンクを付けて表示
             st.subheader(post['content'])
             st.write(post['timestamp'])  # タイムスタンプ
             st.markdown(post_url, unsafe_allow_html=True)
